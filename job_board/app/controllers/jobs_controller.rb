22912659f77669c12b76c834857072ba69aa41d2
Adam McLean <amclean@zoemg.com>
2016-01-09 10:19:34 -0800

    Adds basic actions

--- job_board/app/controllers/jobs_controller.rb
+++ job_board/app/controllers/jobs_controller.rb
@@ -1,2 +1,40 @@
 class JobsController < ApplicationController
+	def index
+	end
+
+	def show
+	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = Job.new(jobs_params)
+
+		if @job.save
+			redirect_to @job
+		else
+			render "New"
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def jobs_params
+		params.require(:job).permit(:title, :description, :company, :url, :address)
+	end
+
+	def find_job
+		@job = Job.find(params[:id])
+	end
 end
